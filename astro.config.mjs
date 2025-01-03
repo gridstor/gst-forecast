ee5c75830db9a833047b2bd9933329ff0d847db1
Administrator <brett.rudder@gridstor.com>
2025-01-03 21:33:12 +0000

    update for netlify build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [
     react(),
     tailwind()
   ],
-  output: 'server'
+  output: 'server',
+  adapter: netlify()
 });
